2a5df65fad99c537a8d45717b719d24e5b6bf2ff
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-11-06 22:00:25 -0800

    Add event listener to button to change dimensions

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,23 +1,14 @@
-/* 
-add a square
-add 16x16 squares
-
-change grid size
-add squares based on grid size
-change squaresize to fit 
-*/
-
 let container = document.querySelector(".container");
-let columns = 16;
-let rows = 16;
-let totalSquares = columns * rows;
+let size = 16;
+let totalSquares = size * size;
 
 function addSquare() {
     let square = document.createElement("div");
 
     square.className = "square";
-    square.style.height = "40px";
     square.style.width = "40px";
+    square.style.height = "40px";
+    
     container.append(square);
 
     square.addEventListener("mouseover", () => {
@@ -28,3 +19,10 @@ function addSquare() {
 for (let i = 0; i < totalSquares; i++) {
     addSquare();
 }
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    size = prompt("Change size of side (1-100)");
+    console.log(size);
+});
+
