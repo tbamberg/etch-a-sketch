8bf5415ff0aa34c6c3f96865eb31d630456fd180
tbamberg <7506119+tbamberg@users.noreply.github.com>
2023-11-06 22:40:36 -0800

    Fix buildGrid() to clear previous grid first

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,23 +1,22 @@
 let container = document.querySelector(".container");
 let squaresPerSide = 16;
 
-
 function addSquare(squareSize) {
     let square = document.createElement("div");
 
     square.className = "square";
     square.style.width = square.style.height = squareSize + "px";
-    
-    container.append(square);
-
     square.addEventListener("mouseover", () => {
         square.style.backgroundColor = "aliceblue";
     });
+
+    container.append(square);
 }
 
 function buildGrid(numSquares) {
+    container.innerHTML = ""; //remove any previous squares
     let totalSquares = numSquares * numSquares;
-    let squareSize = 640 / numSquares; // 640 = width&height of .container
+    let squareSize = 640 / numSquares; //640 = width&height of .container
 
     for (let i = 0; i < totalSquares; i++) {
         addSquare(squareSize);
@@ -29,6 +28,5 @@ buildGrid(squaresPerSide);
 let btn = document.querySelector("button");
 btn.addEventListener("click", () => {
     buildGrid(prompt("Change squares per side (1-100)"));
-    //need to add logic to remove previous grid
 });
 
